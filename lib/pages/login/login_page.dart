4d7bc70e008b3b85e2170661617603440aa1bd91
André Sousa <andrels9283@gmail.com>
2022-06-16 12:55:01 +0100

    Sign in with apple

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 import '../../services/services.dart';
 import '../../shared/shared.dart';
@@ -15,23 +18,55 @@ class LoginPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const FlutterLogo(
-              size: 150,
-            ),
-            LoginButton(
-              icon: FontAwesomeIcons.userNinja,
-              text: 'Continue as Guest',
-              loginMethod: AuthService().anonymLogin,
-              color: Colors.deepPurple,
-            ),
-            LoginButton(
-              text: 'Sign in with Google',
-              icon: FontAwesomeIcons.google,
-              color: Colors.red,
-              loginMethod: AuthService().googleLogin,
-            ),
-          ],
+          children: Platform.isAndroid
+              ? [
+                  const FlutterLogo(
+                    size: 150,
+                  ),
+                  LoginButton(
+                    icon: FontAwesomeIcons.userNinja,
+                    text: 'Continue as Guest',
+                    loginMethod: AuthService().anonymLogin,
+                    color: Colors.deepPurple,
+                  ),
+                  LoginButton(
+                    text: 'Sign in with Google',
+                    icon: FontAwesomeIcons.google,
+                    color: Colors.red,
+                    loginMethod: AuthService().googleLogin,
+                  ),
+                ]
+              : [
+                  const FlutterLogo(
+                    size: 150,
+                  ),
+                  LoginButton(
+                    icon: FontAwesomeIcons.userNinja,
+                    text: 'Continue as Guest',
+                    loginMethod: AuthService().anonymLogin,
+                    color: Colors.deepPurple,
+                  ),
+                  LoginButton(
+                    text: 'Sign in with Google',
+                    icon: FontAwesomeIcons.google,
+                    color: Colors.red,
+                    loginMethod: AuthService().googleLogin,
+                  ),
+                  FutureBuilder<Object>(
+                    future: SignInWithApple.isAvailable(),
+                    builder: (context, snapshot) {
+                      if (snapshot.data == true) {
+                        return SignInWithAppleButton(
+                          onPressed: () {
+                            AuthService().signInWithApple();
+                          },
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                ],
         ),
       ),
     );
